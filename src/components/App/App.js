2c0e52567d677005acb32fbd2e8e994529d48587
Logan <loganed09@gmail.com>
2021-11-12 13:26:21 -0500

    SearchBar and Business list implemented

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,17 @@
 import logo from '../../logo.svg';
+import React from 'react';
 import './App.css';
-import Business from '../Business/Business'
+import Business from '../Business/Business';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar'
 
 function App() {
   return (
-    <Business /> 
+    <div class="App">
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+    </div>
   )
 }
 
---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+
+export default function BusinessList() {
+    return(
+        <div className="BusinessList">
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+        </div>
+    )
+}
