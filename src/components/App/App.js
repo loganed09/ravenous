305bd837a1402aff87d2b9526a869533499bbba8
Logan <loganed09@gmail.com>
2021-11-12 12:55:26 -0500

    Business component added

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import logo from '../../logo.svg';
+import './App.css';
+import Business from '../Business/Business'
+
+function App() {
+  return (
+    <Business /> 
+  )
+}
+
+export default App;
