de3e58c60a746f18b006ea51d29ec27952a0e46e
Logan <loganed09@gmail.com>
2021-11-12 16:27:53 -0500

    cleanup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,27 +6,6 @@ import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar'
 import {Yelp} from '../../util/Yelp';
 
-// const business = {
-//   imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-//   name: 'MarginOtto Pizzeria',
-//   address: '1010 Paddington Way',
-//   city: 'Flavortown',
-//   state: 'NY',
-//   zipCode: '10101',
-//   category: 'Italian',
-//   rating: 4.5,
-//   reviewCount: 90
-// }
-
-// const businesses = [
-//   business,
-//   business,
-//   business,
-//   business,
-//   business,
-//   business
-// ];
-
 function App() {
   const [business, setBusiness] = useState({
     businesses: []
